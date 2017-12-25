6c863c00cc863b738132751806c0c5480f883d77
VladaR87 <v_l_a_d_a_r@yahoo.com>
2017-12-25 02:13:51 +0100

    css changes for responsiveness

--- main.js
+++ main.js
@@ -4,25 +4,22 @@ jQuery(document).ready(function($){
   });
 
   $(".about").click(function() {
+    $('.mobile-nav-wrapper').css('display', 'none');
     $('html, body').animate({
         scrollTop: $("#about").offset().top
-    }, 2000, function(){
-      $('.mobile-nav-wrapper').slideUp();
-    });
+    }, 2000);
   });
   $(".jobs").click(function() {
+    $('.mobile-nav-wrapper').css('display', 'none');
     $('html, body').animate({
         scrollTop: $("#jobs").offset().top - 50
-    }, 2000, function(){
-      $('.mobile-nav-wrapper').slideUp();
-    });
+    }, 2000);
   });
   $(".contact").click(function() {
+    $('.mobile-nav-wrapper').css('display', 'none');
     $('html, body').animate({
         scrollTop: $("#contact").offset().top - 50
-    }, 2000, function(){
-      $('.mobile-nav-wrapper').slideUp();
-    });
+    }, 2000);
   });
 
 });
